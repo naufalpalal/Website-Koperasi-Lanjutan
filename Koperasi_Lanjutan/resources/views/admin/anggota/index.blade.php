5cf42f5c211ce98c541853f0b0357837d4bd8190
leoeka <leoekamatra47770@gmail.com>
2025-09-04 09:21:00 +0700

    icon person kelola anggota

--- Koperasi_Lanjutan/resources/views/admin/anggota/index.blade.php
+++ Koperasi_Lanjutan/resources/views/admin/anggota/index.blade.php
@@ -27,7 +27,10 @@
             <div class="bg-gray-50 border rounded-xl shadow hover:shadow-lg transition p-5">
                 <div class="flex items-center mb-3">
                     <div class="w-12 h-12 flex items-center justify-center rounded-full bg-blue-500 text-white text-2xl">
-                        <i class="bi bi-person"></i>
+                        <svg xmlns="<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                    </svg>
                     </div>
                     <div class="ml-3">
                         <h5 class="text-lg font-bold text-gray-800">{{ $a->nama }}</h5>
