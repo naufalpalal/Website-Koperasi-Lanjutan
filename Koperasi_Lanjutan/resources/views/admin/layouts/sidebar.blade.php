fbd108e982014692e47f1034eb0404ecaa0998cf
leoeka <leoekamatra47770@gmail.com>
2025-09-17 20:42:03 +0700

    nominal simpanan sukarela

--- Koperasi_Lanjutan/resources/views/admin/layouts/sidebar.blade.php
+++ Koperasi_Lanjutan/resources/views/admin/layouts/sidebar.blade.php
@@ -26,11 +26,6 @@
             <nav class="mt-6">
                 <a href="{{ route('admin.dashboard.index') }}"
                     class="flex items-center px-6 py-3 rounded-lg px-4 mb-2 transition hover:bg-blue-700">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-3" fill="none" stroke="currentColor"
-                        viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h18v18H3V3z">
-                        </path>
-                    </svg>
                     Dashboard
                 </a>
                 <a href="{{ route('admin.anggota.index') }}"
