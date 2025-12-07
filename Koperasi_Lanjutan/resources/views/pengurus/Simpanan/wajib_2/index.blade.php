cfc9bbdfa43889479fd185b94494866303f283d3
leoeka <leoekamatra47770@gmail.com>
2025-12-07 22:21:33 +0700

    bug

--- Koperasi_Lanjutan/resources/views/pengurus/Simpanan/wajib_2/index.blade.php
+++ Koperasi_Lanjutan/resources/views/pengurus/Simpanan/wajib_2/index.blade.php
@@ -36,13 +36,13 @@
                 Generate Tagihan
             </button>
             
-            <a href="{{ route('pengurus.simpanan.wajib_2.download', ['bulan' => request('bulan', now()->format('Y-m'))]) }}"
+            <!-- <a href="{{ route('pengurus.simpanan.wajib_2.download', ['bulan' => request('bulan', now()->format('Y-m'))]) }}"
                class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition inline-flex items-center gap-2">
                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/>
                 </svg>
                 Download Tagihan
-            </a>
+            </a> -->
         </div>
     </div>
 
